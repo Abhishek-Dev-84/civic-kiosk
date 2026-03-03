0dae2e75fc6f39781a50ce5bc4df70f2be6ac2a7
Abhishek-Dev-84 <abhishek.nanda1998@gmail.com>
2026-03-03 22:26:32 +0530

    Initial commit

--- Kiosk/settings.py
+++ Kiosk/settings.py
@@ -94,11 +94,10 @@ WSGI_APPLICATION = 'Kiosk.wsgi.application'
 # DATABASE
 # -------------------------------------------------
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
