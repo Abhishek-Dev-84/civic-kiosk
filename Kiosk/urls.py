f0db1d45ddc3a9b79ff63931bf2c96994187587f
Abhishek-Dev-84 <abhishek.nanda1998@gmail.com>
2026-03-06 02:04:46 +0530

    Update Settings.py

--- Kiosk/urls.py
+++ Kiosk/urls.py
@@ -1,21 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.i18n import i18n_patterns, set_language
+from django.conf.urls.i18n import set_language
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+
+    path('admin/', admin.site.urls),
+
     # Language switch
     path('set-language/', set_language, name='set_language'),
+
+    # App URLs
+    path('', include('display.urls')),
 ]
 
-# Language-prefixed URLs
-urlpatterns += i18n_patterns(
-    path('admin/', admin.site.urls),
-    path('', include('display.urls')),
-)
-
-# Serve static & media in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
